41e0a21b209c79313ef87b16bc5755792595310e
alisha <palisha@umich.edu>
2018-11-05 10:21:16 -0500

    added print for coverage decrease

--- rpn.py
+++ rpn.py
@@ -22,12 +22,14 @@ def calculate(myarg):
            # print (colored(stack,'green'))
         except ValueError:
             function = operators[token]
-            print(colored(token,'green'))
+            if(token == '<'):
+                print(token)
+         #   print(colored(token,'green'))
             arg2 = stack.pop()
             arg1 = stack.pop()
             result = function(arg1, arg2)
             stack.append(result)
-        print (colored(stack,'red'))
+       # print (colored(stack,'red'))
     if len(stack) != 1:
         raise TypeError("Too many parameters")
     return stack.pop()
